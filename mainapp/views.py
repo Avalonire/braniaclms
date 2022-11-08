ff4c8402a7933a7cb8ae106084edff68b2dc5e0f
Alex <gofavalon@gmail.com>
2022-11-08 19:14:38 +0400

    Test

--- mainapp/views.py
+++ mainapp/views.py
@@ -92,3 +92,7 @@ class NewsWithPaginatorView(NewsView):
         return context
 
 
+def user(request):
+    age = request.GET.get("age", 0)
+    name = request.GET.get("name", "Undefined")
+    return HttpResponse(f"<h2>Имя: {name}  Возраст: {age}</h2>")
